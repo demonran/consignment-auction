713f9eff7f9359f1e34622f9270c7e7074d9712b
ran.liu1 <ran.liu1@thoughtworks.com>
2021-11-14 20:44:45 +0800

    [story2-AC3-example1-process3]: fake db ,use h2 database mock a not exist data to implement ConsignmentRepository.findByIdOrNull return null.

--- src/integrationTest/kotlin/com/darkhorse/consignmentauction/repository/ConsignmentRepositoryTest.kt
+++ src/integrationTest/kotlin/com/darkhorse/consignmentauction/repository/ConsignmentRepositoryTest.kt
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.jdbc.Sql
 
-class ConsignmentRepositoryTest: IntegrationTest(){
+class ConsignmentRepositoryTest : IntegrationTest() {
 
   @Autowired
   private lateinit var consignmentRepository: ConsignmentRepository
@@ -16,11 +16,19 @@ class ConsignmentRepositoryTest: IntegrationTest(){
   @Sql("/sql/insert_a_consignment_sql.sql")
   fun `should find a consignment when giving an exist id`() {
     val id = "id"
-    val auctionId = "auctionId"
+    val auctionId = "auction_id"
     val consignment = consignmentRepository.findByIdOrNull(id)
 
     assertThat(consignment).isNotNull
     assertThat(consignment?.id).isEqualTo(id)
     assertThat(consignment?.auctionId).isEqualTo(auctionId)
+  }
+
+  @Test
+  fun `should return null when call findByIdOrNull with a not exist id`() {
+    val id = "new_id"
+    val consignment = consignmentRepository.findByIdOrNull(id)
+
+    assertThat(consignment).isNull()
   }
 }
